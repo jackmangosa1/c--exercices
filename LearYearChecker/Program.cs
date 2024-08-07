f94b0f8d5891926ce98643c5229486851a98c5ec
jackmagosa <jackmangosa@gmail.com>
2024-08-07 17:42:05 +0200

    Add Leap year checker

---
+++ LearYearChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LearYearChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) { 
+                Console.WriteLine("The year is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("The year is not a leap year");
+            }
+        }
+    }
+}
