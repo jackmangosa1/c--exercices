e80eeadc8fe07c2228e150ff78b2c358b4959fe5
jackmagosa <jackmangosa@gmail.com>
2024-08-07 16:49:32 +0200

    Add even and odd checker

---
+++ OddAndEvenChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OddAndEvenChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int number  = Convert.ToInt32(Console.ReadLine());
+                if(number % 2 == 0)
+                {
+
+                   Console.WriteLine("The number is even");
+                }
+                else
+                {
+                    Console.WriteLine("The number is odd");
+                }
+
+            }catch(FormatException)
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+}
