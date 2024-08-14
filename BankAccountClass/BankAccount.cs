ecc4860d0aa544bde6a62d88a187a43122a1f2bf
jackmagosa <jackmangosa@gmail.com>
2024-08-14 14:30:10 +0200

    Add part 5

---
+++ BankAccountClass/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountClass
+{
+    internal class BankAccount
+    {
+        public Guid UserId { get; }
+        public string FirstName {get;  set; }
+        public string LastName {get;  set; }
+        public int Balance { get; private set; } = 0;
+
+        public BankAccount(Guid userId, string firstName, string lastName, int balance)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+                return;
+            }
+            Balance += amount;
+            Console.WriteLine($"Successfully deposited {amount}. New balance is {Balance}.");
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be positive.");
+                return;
+            }
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+                return;
+            }
+            Balance -= amount;
+            Console.WriteLine($"Successfully withdrew {amount}. New balance is {Balance}.");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BankAccountClass/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BankAccountClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account  = new BankAccount(Guid.NewGuid(), "Jack", "Mangosa", 100);
+          
+             account.FirstName = "Nicolas";
+             account.LastName = "Kazadi";
+             Console.WriteLine(account.FirstName);
+             Console.WriteLine(account.LastName);
+
+             account.Deposit(100);
+             Console.WriteLine(account.Balance);
+             account.Withdraw(50);
+             
+            account.Deposit(-50);  
+            account.Withdraw(200);  
+            account.Deposit(50);  
+            account.Withdraw(30);
+
+        }
+    }
+}
